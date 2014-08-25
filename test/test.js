89c951f4df6f93e287e5663ae71bfa9f18041ab7
Andrew Butcher <abutcher@redhat.com>
2014-08-24 21:07:42 -0400

    Update tests.

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@
 
 var pulp = require('../node-pulp.js');
 
-//pulp.getRepos(function(d) { console.log(d) });
-//pulp.getRepo('abutcher-test9-re', function(d) { console.log(d) });
-//pulp.getRepoPackages('abutcher-test20-re', function(d) { console.log(d) });
-pulp.searchPackages({"name": {"$regex": ".*httpd.*"}}, function(d) { console.log(d) });
-//pulp.searchRepos({"id": {"$regex": ".*-stage"}}, function(d) { console.log(d) });
+pulp.getRepos(function(d) { console.log(d) });
+pulp.getRepo('abutcher-test9-re', function(d) { console.log(d) });
+pulp.getRepoPackages('abutcher-test20-re', {"unit": ["name", "version"]},function(d) { console.log(d) });
+pulp.searchPackages(["name", "version"], {"name": {"$regex": ".*httpd.*"}}, function(d) { console.log(d) });
+pulp.searchRepos(["display_name"], {"id": {"$regex": ".*-stage"}}, function(d) { console.log(d) });
